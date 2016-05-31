17c57becd1743f0d91dc27b4be4362c0adac016a
gibongestab <giselleblucas@gmail.com>
2016-05-30 21:32:12 -0300

    First commit

---
+++ client/views/main.js
@@ -0,0 +1,14 @@
+Template.layout.onRendered(function() {
+  let giWow = function(selector, effect, offset) {
+    $(selector).waypoint(function() {
+      $(this).addClass('animated ' + effect);
+    }, {
+      offset: offset
+    });
+  };
+
+  /* Animations on scroll */
+
+  giWow('.js-wp-1', 'fadeIn', '50%');
+  giWow('.js-wp-2', 'fadeInUp', '50%');
+});
---
+++ client/views/portfolio/portfolio.js
@@ -0,0 +1,21 @@
+/*
+Template.portfolio.events({
+	'hover .boa-noite': function() {
+		console.log('hover');
+	}
+});
+
+Template.portfolio.onRendered(function() {
+	$('.boa-noite').mouseover(function() {
+		$(this).css('background-color', 'transparent');
+		$(this).css('background-image', 'url("/images/curtas/sonhos.png")');
+		$(this).css('background-size', 'cover');
+	});
+
+	$('.boa-noite').mouseleave(function() {
+		$(this).css('background-color', '#ec685b');
+		$(this).css('background-image', 'none');
+		$(this).css('background-size', 'none');
+	});
+});
+*/
